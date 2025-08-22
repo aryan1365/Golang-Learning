f11133bb6b3a61e758591e38ca7ef1d8abf4be12
aryan1365 <aryan.g@razorpay.com>
2025-08-22 14:46:45 +0530

    for loop implementation

---
+++ 5_four/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	//}
+
+	//infinite loop
+	// for{
+
+	//}
+
+	//classic for loop
+	// 	for i := 0; i < 3; i++ {
+	// 		// break
+	// 		if i == 2 {
+	// 			continue
+	// 		}
+	// 		fmt.Println(i)
+	// 	}
+
+	// }
+	for i := range 3 {
+		fmt.Println(i)
+	}
+
+}
