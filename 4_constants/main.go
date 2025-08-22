d0f91d9913f4f3e16a4d83a379bd40203ec1293f
aryan1365 <aryan.g@razorpay.com>
2025-08-22 14:44:08 +0530

    working with constants

---
+++ 4_constants/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	const name = "Aryan"
+	const age = 20
+	const isAdult = true
+	const (
+		port = 8080
+		host = "localhost"
+	)
+	println(name, age, isAdult, port, host)
+}
