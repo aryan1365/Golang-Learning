7cf9ce677719b4b1053539371d18e357f1ab39c5
aryan1365 <aryan.g@razorpay.com>
2025-08-22 14:41:54 +0530

    variable declaration

---
+++ 3_variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Aryan"
+
+	//golang allows you to declare variables without specifying the type.Itautomattically infers the type based on the value assigned.
+	// var name = "Aryan"
+	name := "Aryan"
+	var isAdult = true
+	fmt.Println(name)
+	name = "Aryan Gupta"
+	fmt.Println(name)
+	fmt.Println(isAdult)
+
+}
