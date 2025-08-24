4f228178108f7bca74931dbdfd29429415f1ea94
aryan1365 <aryan.g@razorpay.com>
2025-08-24 23:10:59 +0530

    database-Configuration

---
+++ Ecommerce-Crud/config/db.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUser := os.Getenv("USER")
+	dbPassword := os.Getenv("PASSWORD")
+	dbHost := os.Getenv("HOST")
+	dbPort := os.Getenv("PORT")
+	dbName := os.Getenv("DBNAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+}
