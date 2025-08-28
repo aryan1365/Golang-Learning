568e1f7e3d6899c6957ef08c69924038bec29bb5
aryan1365 <aryan.g@razorpay.com>
2025-08-28 15:54:36 +0530

    Init Use

---
+++ Init/Init.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initilizing package1")
+}
+func init() {
+	fmt.Println("Initilizing package2")
+}
+func init() {
+	fmt.Println("Initilizing package3")
+}
+
+func main() {
+	fmt.Println("Main fn")
+}
